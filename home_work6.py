77da680e1fe2b896ddd7e1ca4b2711ea73df7dd8
hasanmizaushev <62481070+hasanmizaushev@users.noreply.github.com>
2020-09-24 10:47:44 +0300

    Add files via upload

---
+++ home_work6.py
@@ -0,0 +1,18 @@
+def number_1():
+    swine_flu_probability = 99 / 100
+    return print('you have swine flu, if the test is positive, it is', swine_flu_probability)
+number_1()
+
+
+def numder_2():
+    return print('the new probability of a positive test result will be equal to the old 0.99')
+numder_2()
+
+
+def numder_3():
+    the_likelihood_that_you_will_be_sick = 1 / 350
+    test_accuracy = 100 - 2
+    swine_flu_probability = test_accuracy / 100
+    the_outcome = the_likelihood_that_you_will_be_sick * swine_flu_probability
+    return print('you have swine flu, if the test is positive, it is', the_outcome)
+numder_3()
