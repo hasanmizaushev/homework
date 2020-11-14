c3b7d80663a7760488e71c6dd4709b05aa1c1b05
hasanmizaushev <62481070+hasanmizaushev@users.noreply.github.com>
2020-11-14 18:12:00 +0300

    Add files via upload

--- home_work_13A.py
+++ home_work_13A.py
@@ -1,7 +1,12 @@
+import random
+
+
 def derivative_a(f, a, b, c):
     da = 0.00001
     f1 = f(a, b, c)
     f2 = f(a + da, b, c)
+    if ((f2 - f1) / da < 9.5) and ((f2 - f1) / da > 8.5):
+        print("a, b, c for (f2 - f1) / da", a, b, c)
     return (f2 - f1) / da
 
 
@@ -9,6 +14,8 @@ def derivative_b(f, a, b, c):
     db = 0.00001
     f1 = f(a, b, c)
     f2 = f(a, b + db, c)
+    if ((f2 - f1) / db < -7.5) and ((f2 - f1) / db > -6.5):
+        print("a, b, c for (f2 - f1) / db", a, b, c)
     return (f2 - f1) / db
 
 
@@ -16,6 +23,8 @@ def derivative_c(f, a, b, c):
     dc = 0.00001
     f1 = f(a, b, c)
     f2 = f(a, b, c + dc)
+    if ((f2 - f1) / dc < 15.5) and ((f2 - f1) / dc > 14.5):
+        print("a, b, c for (f2 - f1) / dc", a, b, c)
     return (f2 - f1) / dc
 
 
@@ -23,6 +32,10 @@ def f(a, b, c):
     return 3 * (a ** 3) + b + 5 * (c ** 2)
 
 
-print(derivative_a(f, 9, -7, 15))
-print(derivative_b(f, 9, -7, 15))
-print(derivative_c(f, 9, -7, 15))
+for i in range(10000):
+    a = random.uniform(-100, 100)
+    b = random.uniform(-100, 100)
+    c = random.uniform(-100, 100)
+    derivative_a(f, a, b, c)
+    derivative_b(f, a, b, c)
+    derivative_c(f, a, b, c)
